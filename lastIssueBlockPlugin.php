d4b4e25d15bb67aeb10a463c1aff1cf5b7ea54a0
parinisantiago <parinisantiago@gmail.com>
2017-05-18 11:06:29 -0300

    added function getContents

--- lastIssueBlockPlugin.php
+++ lastIssueBlockPlugin.php
@@ -19,6 +19,30 @@ class LastIssueBlockPlugin extends BlockPlugin {
 	function getDescription() {
 		return __('plugins.block.lastIssue.description');
 	}
+
+    function getContents($templateMgr, $request = null) {
+        $issueDao = DAORegistry::getDAO('IssueDAO');
+        $journalDao = DAORegistry::getDAO('JournalDAO');
+		$lastIssues = array();
+        $journals = $journalDao->getAll(true, null)->toArray();
+        foreach ($journals as $journal){
+        	$lastIssue = array();
+        	$issue = $issueDao->getCurrent($journal->getId());
+        	if($issue)
+        	{
+                $lastIssue['journal'] = $journal->getLocalizedName();
+                $lastIssue['path'] = $journal->getPath();
+                $lastIssue['issue'] = $issue->getLocalizedTitle();
+                $lastIssue['published'] = $issue->getDatePublished();
+                array_push($lastIssues, $lastIssue);
+            }
+		}
+        $templateMgr->assign(array(
+			'issues' => $lastIssues
+        ));
+
+        return parent::getContents($templateMgr, $request);
+    }
 }
 
 ?>
